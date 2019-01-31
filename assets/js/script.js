e176a86a0644200cb1ea3235c00fd4d54187406e
Kpix <c.castillocontr@gmail.com>
2019-01-31 13:56:39 -0300

    13th commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
 $(function() {
 	
-	$('.mainphoto__img').hover(function() {
+	$('.mainside__img img').hover(function() {
 		$(this)
 			.toggleClass('transparency', '1.2s')
 	})
 
-	
+
+	$('.brand__krustyburguer a i').on('click', function(event) {
+		event.preventDefault();
+		$('.brand__menu li').toggleClass('show')
+	})	
 
 });
